4b40c5d7d5b469ed38338bdb51fb29c55450b256
Kunyatta <kunyatta@gmail.com>
2026-01-22 10:24:26 +0900

    カットインのマニフェストを修正

--- src/app/plugins/cut-in/manifest.ts
+++ src/app/plugins/cut-in/manifest.ts
@@ -4,7 +4,7 @@ export const MANIFEST: PluginManifest = {
   id: 'cut-in-plugin',
   name: 'カットイン',
   version: '1.1.0',
-  description: 'チャットの発言に合わせて画面全体にカットイン画像を演出します。\n\n2026/01/22 UIを更新',
+  description: 'チャットの発言に合わせて画面全体にカットイン画像を演出します。\n\n2026/01/22 UIを更新\n2026/01/22 配置ガイド表示に対応。演出詳細のUIも修正',
   icon: 'burst_mode',
   isExperimental: false
 };
